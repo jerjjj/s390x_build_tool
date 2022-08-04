3f8ebdf8fc8262042f44cc40b91059a7b888b85b
jerjjj <wuzhijun@jerjjj.cn>
2022-08-04 18:41:35 +0800

    update
    修复了部分命令权限不够的bug
    增加mysql的编译

--- s390x-build.sh
+++ s390x-build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo ----------------------------------------------------------------脚本仅供个人使用----------------------------------------------------------------
 echo ----------------------------------------------------------------创建工作目录----------------------------------------------------------------
-echo ----------------------------------------------------------------安装一些依赖----------------------------------------------------------------
-sudo apt install gcc make openssl curl libssl-dev libxml2-dev libzip-dev libcurl4-openssl-dev libpng-dev libjpeg-dev \
-	libwebp-dev libonig-dev libsqlite3-dev libsodium-dev libargon2-dev
+sudo su
 mkdir s390x-build
+echo ----------------------------------------------------------------安装一些依赖----------------------------------------------------------------
+apt install gcc make openssl curl libssl-dev libxml2-dev libzip-dev libcurl4-openssl-dev libpng-dev libjpeg-dev \
+	libwebp-dev libonig-dev libsqlite3-dev libsodium-dev libargon2-dev
+apt -y install make cmake gcc g++ perl bison libaio-dev libncurses5 libncurses5-dev libnuma-dev libssl-dev
 echo ----------------------------------------------------------------开始拉取python源码----------------------------------------------------------------
 cd s390x-build
 git clone https://github.com/python/cpython.git -b main
@@ -28,7 +30,7 @@ cd node
 ./configure
 make
 echo ----------------------------------------------------------------安装nodejs----------------------------------------------------------------
-sudo make install
+make install
 echo ----------------------------------------------------------------nodejs安装完成----------------------------------------------------------------
 echo ----------------------------------------------------------------开始下载nginx源码----------------------------------------------------------------
 cd ~/s390x-build/
@@ -42,9 +44,9 @@ cd nginx
 ./configure --prefix=/usr/local/nginx --with-http_ssl_module --with-stream --with-mail=dynamic
 make
 echo ----------------------------------------------------------------安装nginx----------------------------------------------------------------
-sudo make install
-sudo ln -s /opt/dotnet/dotnet /usr/local/bin
-sudo /usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
+make install
+ln -s /opt/dotnet/dotnet /usr/local/bin
+/usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
 echo ----------------------------------------------------------------nginx安装完成----------------------------------------------------------------
 echo ----------------------------------------------------------------开始下载PHP源码----------------------------------------------------------------
 cd ~/s390x-build/
@@ -74,15 +76,47 @@ cd php
 	--enable-shmop \
 	--enable-sockets \
 	--enable-mysqlnd \
-	--disable-cgi
-    
+	--disable-cgi    
 make
 echo ----------------------------------------------------------------安装PHP----------------------------------------------------------------
-sudo make insatll
+make insatll
 echo ----------------------------------------------------------------配置PHP----------------------------------------------------------------
-sudo cp php.ini-production /usr/local/etc/php.ini
-sudo mv /usr/local/etc/php-fpm.conf.default /usr/local/etc/php-fpm.conf
-sudo mv /usr/local/etc/php-fpm.d/www.conf.default /usr/local/etc/php-fpm.d/www.conf
-sudo echo "pid = run/php-fpm.pid" >> /usr/local/etc/php-fpm.conf
-sudo sed -i "s/listen = 127.0.0.1:9000/listen = var\/run\/php-fpm.sock\//" /usr/local/etc/php-fpm.d/www.conf
+cp php.ini-production /usr/local/etc/php.ini
+mv /usr/local/etc/php-fpm.conf.default /usr/local/etc/php-fpm.conf
+mv /usr/local/etc/php-fpm.d/www.conf.default /usr/local/etc/php-fpm.d/www.conf
+echo "pid = run/php-fpm.pid" >> /usr/local/etc/php-fpm.conf
+sed -i "s/listen = 127.0.0.1:9000/listen = var\/run\/php-fpm.sock\//" /usr/local/etc/php-fpm.d/www.conf
 echo ----------------------------------------------------------------PHP安装完成----------------------------------------------------------------
+echo ----------------------------------------------------------------开始下载MySQL源码----------------------------------------------------------------
+cd ~/s390x-build/
+wget https://downloads.mysql.com/archives/get/p/23/file/mysql-boost-5.7.38.tar.gz
+echo ----------------------------------------------------------------开始解压MySQL源码----------------------------------------------------------------
+tar zxvf mysql-boost-5.7.38.tar.gz
+mv mysql-5.7.38 mysql
+echo ----------------------------------------------------------------准备编译MySQL源码----------------------------------------------------------------
+groupadd mysql
+useradd -r -g mysql mysql
+rm -rf /opt/mysql
+mkdir -p /opt/mysql/install
+mkdir -p /opt/mysql/data
+mkdir -p /opt/mysql/log
+chown -R mysql:mysql /opt/mysql
+echo ----------------------------------------------------------------编译MySQL----------------------------------------------------------------
+cd mysql
+cmake \
+-DCMAKE_INSTALL_PREFIX=/opt/mysql/install \
+-DMYSQL_UNIX_ADDR=/opt/mysql/install/mysql.sock \
+-DDEFAULT_CHARSET=utf8 \
+-DDEFAULT_COLLATION=utf8_general_ci \
+-DWITH_INNOBASE_STORAGE_ENGINE=1 \
+-DWITH_ARCHIVE_STORAGE_ENGINE=1 \
+-DWITH_BLACKHOLE_STORAGE_ENGINE=1 \
+-DMYSQL_DATADIR=/opt/mysql/data \
+-DMYSQL_TCP_PORT=3306 \
+-DWITH_BOOST=boost
+make -j2
+make install
+chown -R mysql:mysql /opt/mysql
+chgrp -R mysql /opt/mysql
+echo "PATH=$PATH:/opt/mysql/install/bin" >> /etc/profile
+echo ----------------------------------------------------------------MySQL安装完成(未初始化)----------------------------------------------------------------
